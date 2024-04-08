8f8e9ec956384fb73eea0d7ea4a6a2e5b6938ac3
croissain <croissain@croissains-Mac-mini.local>
2024-04-08 18:22:53 +0700

    update about resp

--- src/modules/About/components/Pipeline/index.tsx
+++ src/modules/About/components/Pipeline/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Box,
   GridItem,
@@ -9,17 +11,17 @@ import {
 import Image from "next/image";
 
 import { pipelineData } from "@/constants/pipelineData";
+import useWindowSize from "@/hooks/common/useWindowSize";
 
 import s from "./style.module.scss";
 
 export default function AboutPipeline(): React.ReactElement {
+  const { isMobile } = useWindowSize();
   return (
     <Stack className={s.pipeline}>
       <VStack className={s.pipeline_title}>
-        <Text fontSize={42} fontWeight={600}>
-          Our data pipeline
-        </Text>
-        <Text fontSize={16}>
+        <Text fontSize={{ base: 24, sm: 24, md: 48 }}>Our data pipeline</Text>
+        <Text fontSize={{ base: 14, sm: 14, md: 16 }}>
           Through our integrated deep data supply chain, we provide accurate and
           detailed data.
         </Text>
@@ -29,13 +31,15 @@ export default function AboutPipeline(): React.ReactElement {
         columns={{ base: 1, sm: 2, md: 4 }}
         className={s.pipeline_list}
       >
-        <GridItem>
-          <Stack>
-            <Box className={s.pipeline_item_img}>
-              <Image src="/images/0.png" alt="" fill />
-            </Box>
-          </Stack>
-        </GridItem>
+        {!isMobile && (
+          <GridItem>
+            <Stack>
+              <Box className={s.pipeline_item_img}>
+                <Image src="/images/0.png" alt="" fill />
+              </Box>
+            </Stack>
+          </GridItem>
+        )}
         {pipelineData.map((item, index) => (
           <GridItem key={item.label}>
             <Stack>
