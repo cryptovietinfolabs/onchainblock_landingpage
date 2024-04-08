17f13713fe955505764f1732fd5bb94b39b1e411
croissain <croissain@croissains-Mac-mini.local>
2024-04-08 08:43:42 +0700

    update pipeline images

--- src/constants/pipelineData.ts
+++ src/constants/pipelineData.ts
@@ -2,13 +2,16 @@ export const pipelineData = [
   {
     label: "Extract & Load",
     desc: "We operate a dedicated node infrastructure to reliably extract raw data from public blockchains.",
+    image: "/images/1.png",
   },
   {
     label: "Transform",
     desc: "Extracting raw data from reliable public chains through the operation of dedicated node infrastructure.",
+    image: "/images/2.png",
   },
   {
     label: "Service",
     desc: "We deliver standardized data to customers through our own web application and API.",
+    image: "/images/3.png",
   },
 ];
